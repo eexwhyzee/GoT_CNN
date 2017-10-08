a6e247654a2b1fd77a28aab7a02e8630f4010797
eexwhyzee <mhoang08@gmail.com>
2017-10-08 01:38:39 -0700

    Fixed indent issue

--- GoT_utils.py
+++ GoT_utils.py
@@ -20,20 +20,20 @@ def load_samples(path):
 	return paths
 
 def load_dataset(path):
-    data = load_files(path)
-    GoT_files = np.array(data['filenames'])
-    GoT_targets = np_utils.to_categorical(np.array(data['target']), 2)
-    return GoT_files, GoT_targets
+	data = load_files(path)
+	GoT_files = np.array(data['filenames'])
+	GoT_targets = np_utils.to_categorical(np.array(data['target']), 2)
+	return GoT_files, GoT_targets
 
 def path_to_tensor(img_path):
-    '''loads RGB image as PIL.Image.Image type'''
-    img = image.load_img(img_path, target_size=(224, 224))
-    # convert PIL.Image.Image type to 3D tensor with shape (224, 224, 3)
-    x = image.img_to_array(img)
-    # convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
-    return np.expand_dims(x, axis=0)
+	'''loads RGB image as PIL.Image.Image type'''
+	img = image.load_img(img_path, target_size=(224, 224))
+	# convert PIL.Image.Image type to 3D tensor with shape (224, 224, 3)
+	x = image.img_to_array(img)
+	# convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
+	return np.expand_dims(x, axis=0)
 
 def paths_to_tensor(img_paths):
-    list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
-    return np.vstack(list_of_tensors)
+	list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
+	return np.vstack(list_of_tensors)
 
